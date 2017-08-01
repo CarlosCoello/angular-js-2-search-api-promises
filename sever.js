a0fd4e2dde5f6ae0cc5d08aea15fd36a6f0e4aa5
Carlos Coello <carlos.coello05@outlook.com>
2017-08-01 03:19:59 -0500

    changes to server.js

--- sever.js
+++ sever.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use( cors());
+app.use( expres.static(__dirname + '/dist'));
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
